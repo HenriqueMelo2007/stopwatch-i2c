aee324a03e3193d3f60cfa29a4cd89e41cfa90de
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-04-05 17:48:37 -0300

    feat: it adds the final code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define COLUMNS 16
+#define LINES 2
+#define ADDRESS 0x20
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+unsigned long previousMillis = 0;
+unsigned int seconds = 0;
+unsigned int minutes = 0;
+unsigned int hours = 0;
+
+LiquidCrystal_I2C lcd(ADDRESS, COLUMNS, LINES);
+
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Stopwatch");
+  lcd.setCursor(13, 0);
+  lcd.print("C++");
+  lcd.setCursor(0, 1);
+  lcd.print("00:00:00");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  unsigned long currentMillis = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (currentMillis - previousMillis >= 1000) {
+    previousMillis = currentMillis;
+    seconds++;
+
+    if (seconds > 59) {
+      seconds = 0;
+      minutes++;
+      if (minutes > 59) {
+        minutes = 0;
+        hours++;
+      }
+    }
+
+    lcd.setCursor(0, 1);
+    if (hours < 10) lcd.print("0");
+    lcd.print(hours);
+    lcd.print(":");
+
+    if (minutes < 10) lcd.print("0");
+    lcd.print(minutes);
+    lcd.print(":");
+
+    if (seconds < 10) lcd.print("0");
+    lcd.print(seconds);
+  }
 }
